95f42ac12e0fc763bb1bfa1c52cfdc25ea792fb5
Rainer Staude <rainer.staude@gmx.de>
2022-02-24 20:06:15 +0100

    remove debug message

--- __init__.py
+++ __init__.py
@@ -8,8 +8,6 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup(hass, config):
     """Initiate becker component for home assistant."""
 
-    _LOGGER.debug("Set up the becker platform - async_setup")
-
     # Register this component's services
     await PyBecker.async_register_services(hass)
 
